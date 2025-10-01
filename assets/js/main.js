be649ee1cad866ae62e3fc677f275690f7a27f65
Rahat-Hridoy <rahathridoyd2x@gmail.com>
2025-10-02 03:11:25 +0600

    ADD: swiper on card

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,38 @@
-var swiper = new Swiper(".mySwiper", {
-  slidesPerView: 4,
-  spaceBetween: 30,
-  centeredSlides: true,
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
-});
+// Hero Card
+const initMySwiperHero = function () {
+  var swiper = new Swiper(".mySwiperHero", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+      delay: 5000,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    breakpoint: {
+      768: { slidesPerView: 1 },
+      1400: { slidesPerView: 3 },
+    },
+  });
+};
+
+const initMySwiper = function () {
+  const isMobile = window.matchMedia("max-width: 1400px").matches;
+  var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 4,
+    spaceBetween: 30,
+    loop: isMobile,
+    centeredSlides: true,
+    // pagination: {
+    //   el: ".swiper-pagination",
+    //   clickable: false,
+    // },
+    breakpoint: {
+      768: { slidesPerView: 2 },
+      1024: { slidesPerView: 4 },
+      1400: { slidesPerView: 5 },
+    },
+  });
+};
